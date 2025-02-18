1dbad4cec6a1045be2f286a6578530e86cea56f0
vanthao9x <levanthaolvt228@gmail.com>
2025-02-18 14:58:47 +0700

    Gv 38 finish create user api for admin (#17)
    * merge dev
    
    * create user
    
    * finish create user
    
    * fix/add role id
    
    * fix/update status request

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '@prisma/client';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -9,6 +12,67 @@ export class UsersService {
     return this.prisma.user.findMany({
       where: { is_active: true },
       orderBy: { created_at: 'desc' },
+    });
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const {
+      username,
+      email,
+      phone_number,
+      role_id,
+      ward_id,
+      preferences,
+      is_active,
+    } = createUserDto;
+
+    // Kiểm tra username đã tồn tại
+    const existingUserByUsername = await this.prisma.user.findUnique({
+      where: { username },
+    });
+    if (existingUserByUsername) {
+      throw new BadRequestException('Username đã tồn tại');
+    }
+
+    // Kiểm tra email đã tồn tại nếu có email
+    if (email) {
+      const existingUserByEmail = await this.prisma.user.findUnique({
+        where: { email },
+      });
+      if (existingUserByEmail) {
+        throw new BadRequestException('Email đã tồn tại');
+      }
+    }
+
+    // Kiểm tra phone_number đã tồn tại nếu có phone_number
+    if (phone_number) {
+      const existingUserByPhone = await this.prisma.user.findUnique({
+        where: { phone_number },
+      });
+      if (existingUserByPhone) {
+        throw new BadRequestException('Số điện thoại đã tồn tại');
+      }
+    }
+
+    // Nếu không có cả email và phone_number thì báo lỗi
+    if (!email && !phone_number) {
+      throw new BadRequestException('Bạn chưa điền email hoặc số điện thoại');
+    }
+
+    if (!role_id) {
+      throw new BadRequestException('Bạn chưa chọn role id');
+    }
+
+    return this.prisma.user.create({
+      data: {
+        username,
+        email,
+        phone_number,
+        role_id,
+        ward_id,
+        preferences,
+        is_active,
+      },
     });
   }
 
