09875c641dded2b15f3e01d338f2032596639381
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2025-02-18 08:38:10 +0700

    fix status update plant

--- src/plants/plants.service.ts
+++ src/plants/plants.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 import { CreatePlantDto } from './dto/create-plant.dto';
 import { UpdatePlantDto } from './dto/update-plant.dto';
@@ -47,7 +47,7 @@ export class PlantsService {
       });
 
       if (!plant) {
-        throw new Error(`Plant with id ${id} not found`);
+        throw new NotFoundException(`Plant with id ${id} not found`);
       }
 
       const updatedPlant = await this.prisma.plant.update({
@@ -58,6 +58,9 @@ export class PlantsService {
       });
       return updatedPlant;
     } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
       throw new Error(`Failed to update plant with id ${id}: ${error.message}`);
     }
   }
