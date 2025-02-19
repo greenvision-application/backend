a8b6e6c7443a9d4496b1f6388772efe236128c27
vanthao9x <levanthaolvt228@gmail.com>
2025-02-19 08:59:40 +0700

    finish update User (#18)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
 import { BadRequestException } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -100,6 +101,63 @@ export class UsersService {
     return this.prisma.user.update({
       where: { id },
       data: { is_active: false },
+    });
+  }
+
+  /*************  ✨ Codeium Command ⭐  *************/
+  /**
+   * Update a user by its id
+   *
+   * @param id ID of the user to be updated
+   * @param updateUserDto Data to be updated
+   * @returns The updated user
+   * @throws {NotFoundException} if the user with the given id does not exist
+   */
+  /******  57870a8b-0474-4e8d-b4b7-a5be59d18391  *******/
+  async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    const { username, email, phone_number } = updateUserDto;
+
+    // Kiểm tra username đã tồn tại (ngoại trừ user hiện tại)
+    if (username) {
+      const existingUserByUsername = await this.prisma.user.findUnique({
+        where: { username },
+      });
+      if (existingUserByUsername && existingUserByUsername.id !== id) {
+        throw new BadRequestException('Username đã tồn tại');
+      }
+    }
+
+    // Kiểm tra email đã tồn tại (ngoại trừ user hiện tại)
+    if (email) {
+      const existingUserByEmail = await this.prisma.user.findUnique({
+        where: { email },
+      });
+      if (existingUserByEmail && existingUserByEmail.id !== id) {
+        throw new BadRequestException('Email đã tồn tại');
+      }
+    }
+
+    // Kiểm tra phone_number đã tồn tại (ngoại trừ user hiện tại)
+    if (phone_number) {
+      const existingUserByPhone = await this.prisma.user.findUnique({
+        where: { phone_number },
+      });
+      if (existingUserByPhone && existingUserByPhone.id !== id) {
+        throw new BadRequestException('Số điện thoại đã tồn tại');
+      }
+    }
+
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
     });
   }
 }
