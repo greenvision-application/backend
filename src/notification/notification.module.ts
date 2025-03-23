7c9a82650cb39997cbdc64f68c1f00efe0a9d408
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2025-03-23 16:05:07 +0700

    add simple notification

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { PrismaService } from '@/prisma/prisma.service';
+import { TaskNotificationService } from './check-task.service';
 
 @Module({
   controllers: [NotificationController],
-  providers: [NotificationService, PrismaService],
+  providers: [NotificationService, PrismaService, TaskNotificationService],
 })
 export class NotificationModule {}
