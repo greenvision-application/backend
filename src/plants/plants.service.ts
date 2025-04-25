18efda45a9c662b968ddc319634ddd6492e5d922
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2025-04-25 13:05:52 +0700

    add category id

--- src/plants/plants.service.ts
+++ src/plants/plants.service.ts
@@ -117,6 +117,7 @@ export class PlantsService {
           approved_content: true,
           Category: {
             select: {
+              id: true,
               category_name: true,
             },
           },
